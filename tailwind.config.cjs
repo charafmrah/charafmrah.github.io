b62d65eb18962b9ddf49ff5874a5e5f39c6b462c
Charaf Mrah <charaf4charaf@gmail.com>
2022-10-16 23:33:17 +0300

    fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        MajorMonoDisplay: ["Major Mono Display", "monospace"],
+        SpaceMono: ["Space Mono", "monospace"],
+      },
+      colors: {
+        secondary: "#28282B",
+      },
+    },
   },
   plugins: [],
 };
