59d1a315798976b73ef00cb828170a9001781e58
charafmrah <charaf4charaf@gmail.com>
2022-12-26 22:30:19 +0300

    update base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://charafmrah.github.io",
-  base: "/charaf-mrah-portfolio",
   integrations: [mdx(), sitemap()],
 });
