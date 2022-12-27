032335971f67d1ad55b6ee4c9b67d249c64af7f8
charafmrah <charaf4charaf@gmail.com>
2022-12-28 00:42:20 +0300

    nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://charafmrah.github.io",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()]
 });
