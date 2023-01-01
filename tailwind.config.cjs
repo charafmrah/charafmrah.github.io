9ddb3c32b4ffbd8b415b68dc56f3b163a1ea81d3
charafmrah <charaf4charaf@gmail.com>
2023-01-01 18:48:13 +0300

    fonts & styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+      },
+      backgroundImage: {
+        "body-background": "url('/src/images/background.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
