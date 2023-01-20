b98a6fc27e0400bdab51a5ccdadc6e9519304604
charafmrah <charaf4charaf@gmail.com>
2023-01-20 17:34:47 +0300

    bug fix & logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,14 +9,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 // https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
-
-// https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://charafmrah.github.io",
-  integrations: [mdx(), sitemap(), tailwind(), react(), image()]
+  integrations: [mdx(), sitemap(), tailwind(), react()],
 });
