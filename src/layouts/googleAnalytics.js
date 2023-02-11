3932f5d670c3182c13763469b05a7ea551475a36
charafmrah <charaf4charaf@gmail.com>
2023-02-11 19:58:00 +0100

    update

--- src/layouts/googleAnalytics.js
+++ src/layouts/googleAnalytics.js
@@ -1,8 +1,6 @@
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-59JLRZ4JB2"></script>
-<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-59JLRZ4JB2');
-</script>
