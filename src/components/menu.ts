a16ea9d94683edd7ab8b9f0753497754ad843822
charafmrah <charaf4charaf@gmail.com>
2023-02-12 09:56:20 +0100

    update

--- src/components/menu.ts
+++
@@ -1,8 +0,0 @@
-document.getElementById("nav-toggle").addEventListener("click", toggleNav);
-
-function toggleNav(): void {
-document.body.dataset.nav =
-    document.body.dataset.nav === "true" ? "false" : "true";
-}
-
-export {}
