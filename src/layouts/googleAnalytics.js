6ffffdfa841a79076156b2b58d21d8c734ce9e66
charafmrah <charaf4charaf@gmail.com>
2023-02-12 12:59:49 +0100

    rm

--- src/layouts/googleAnalytics.js
+++
@@ -1,6 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-59JLRZ4JB2"></script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-59JLRZ4JB2');
