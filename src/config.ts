f89638ad2c2cf3bb40a9b789e558cf751dd51aa2
charafmrah <charaf4charaf@gmail.com>
2023-02-17 12:41:38 +0100

    more SEO + removed fontawesome

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Charaf Mrah';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'A creative software engineer with a passion for building beautiful and performant web applications.';
