a20fe7df5f3858795f7ef3cfc85aa058756b33c9
charafmrah <charaf4charaf@gmail.com>
2023-02-18 13:47:57 +0100

    badges

--- src/components/home/MovingCards/PortfolioData.ts
+++ src/components/home/MovingCards/PortfolioData.ts
@@ -25,6 +25,11 @@ export const cardsData = [
       "An Android app that helps users find the most efficient route through a network of airports. ",
     projectUrl: "https://github.com/charafmrah/flight-planning-system",
     bgColor: "#22d3ee",
+    technologies: [
+      "https://img.shields.io/badge/java-007396?style=for-the-badge&logo=java&logoColor=white",
+      "https://img.shields.io/badge/kotlin-0095D5?style=for-the-badge&logo=kotlin&logoColor=white",
+      "https://img.shields.io/badge/Android-3DDC84?style=for-the-badge&logo=android&logoColor=white",
+    ],
   },
   {
     id: 3,
@@ -34,6 +39,9 @@ export const cardsData = [
       "Applying machine learning algorithms to classify military network connections as normal or anomalous. ",
     projectUrl: "https://github.com/charafmrah/network-intrusion-detection",
     bgColor: "#f87171",
+    technologies: [
+      "https://img.shields.io/badge/Python-ffe873?style=for-the-badge&logo=python",
+    ],
   },
   {
     id: 4,
@@ -43,6 +51,14 @@ export const cardsData = [
       "An IoT device & Next Js website to control & visualize the data using AWS.",
     projectUrl: "https://github.com/charafmrah/home-health-monitor",
     bgColor: "#34d399",
+    technologies: [
+      "https://img.shields.io/badge/next.js-000000?style=for-the-badge&logo=nextdotjs&logoColor=white",
+      "https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB",
+      "https://img.shields.io/badge/Tailwind-563D7C?style=for-the-badge&logo=tailwindcss&logoColor=white",
+      "https://img.shields.io/badge/JavaScript-f7df1e?style=for-the-badge&logo=javascript&logoColor=black",
+      "https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white",
+      "https://img.shields.io/badge/Amazon%20AWS-232F3E?style=for-the-badge&logo=amazonaws&logoColor=white",
+    ],
   },
   {
     id: 5,
@@ -51,6 +67,10 @@ export const cardsData = [
     description: "A blog website for college students.",
     projectUrl: "https://thecollegeroute.com",
     bgColor: "#a78bfa",
+    technologies: [
+      "https://img.shields.io/badge/php-777BB4?style=for-the-badge&logo=php&logoColor=white",
+      "https://img.shields.io/badge/WordPress-21759B?style=for-the-badge&logo=wordpress&logoColor=white",
+    ],
   },
   {
     id: 6,
@@ -59,5 +79,12 @@ export const cardsData = [
     description: "My portfolio website.",
     projectUrl: "https://charafmrah.com",
     bgColor: "#fbbf24",
+    technologies: [
+      "https://img.shields.io/badge/Astro-ff5a1f?style=for-the-badge&logo=astro&logoColor=white",
+      "https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white",
+      "https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB",
+      "https://img.shields.io/badge/Tailwind-563D7C?style=for-the-badge&logo=tailwindcss&logoColor=white",
+      "https://img.shields.io/badge/JavaScript-f7df1e?style=for-the-badge&logo=javascript&logoColor=black",
+    ],
   },
 ];
